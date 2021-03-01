fbecd40ecdba0b1bb4cec0c48bba57fb4e6a89b8
Brian Kennedy <bpkennedy@gmail.com>
2021-03-01 16:39:15 -0600

    Initial commit with docker-structure for project

---
+++ backend/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('tests', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    return response()->json([
+       'name' => 'Abigail',
+       'state' => 'CA',
+   ]);
+});
