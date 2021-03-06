9ebf00ab866c99e2043f776c168977550949ee52
Brian Kennedy <bpkennedy@gmail.com>
2021-03-06 17:34:25 -0600

    Change bashisms to POSIX style

--- scripts/push-images.sh
+++ scripts/push-images.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 # This script pushes EXISTING images to Docker Hub - this script is only meant for PBBG Maintainers to use!
-# Development Usage: ./push-images.sh -username myUserName -password myPassword
-# Production Usage: ./push-images.sh -username myUserName -password myPassword -production true
+# Development Usage: ./push-images.sh --username myUserName --password myPassword
+# Production Usage: ./push-images.sh --username myUserName --password myPassword --production true
 
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
@@ -10,31 +10,28 @@ CYAN='\033[1;36m'
 NC='\033[0m' # No Color
 
 echo "==== push-images.sh script ===="
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-    -username)
-        username=$2
-        shift
-        ;;
-    -password)
-        password=$2
-        shift
-        ;;
-    -production)
-        production=$2
-        shift
-        ;;
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --username=*)
+      username="${1#*=}"
+      ;;
+    --password=*)
+      password="${1#*=}"
+      ;;
+    --production=*)
+      production="${1#*=}"
+      ;;
     *)
-        echo "Invalid argument: $1"
-        exit 1
-    esac
-    shift
+      printf "* Error: Invalid argument.*\n"
+      exit 1
+  esac
+  shift
 done
 
 echo "Logging into PBBG.com Docker Hub"
 echo $password | docker login -u $username --password-stdin
 
-if [[ -n "$production" ]]; then
+if [ -n "$production" ]; then
   echo "${CYAN}Pushing production images to PBBG.com Docker Hub${NC}"
   docker-compose -f docker-compose.build-for-prod.yml push
 else
