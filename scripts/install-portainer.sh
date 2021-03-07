d0c813b0197ff75664edeba1cb8f06650e9e9a98
Brian Kennedy <bpkennedy@gmail.com>
2021-03-06 23:27:34 -0600

    Add timestamps to shell script output

--- scripts/install-portainer.sh
+++ scripts/install-portainer.sh
@@ -6,16 +6,16 @@ set -e
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
-echo "==== install-portainer.sh script ===="
+echo $(date -u) "==== install-portainer.sh script ===="
 
-echo "Using bcrypt runtime docker container to create an encrypted password..."
+echo $(date -u) "Using bcrypt runtime docker container to create an encrypted password..."
 # Initial password is bcrypt encrypted password 'admin' using one-time run of httpd:2.4-alpine container
 password=docker run --rm httpd:2.4-alpine htpasswd -nbB admin "admin" | cut -d ":" -f 2
 
-echo "Creating docker volume for Portainer data..."
+echo $(date -u) "Creating docker volume for Portainer data..."
 docker volume create portainer_data
 
-echo "Starting Portainer.io standalone container, listening on port 9000..."
+echo $(date -u) "Starting Portainer.io standalone container, listening on port 9000..."
 docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce --admin-password=$password
-echo "${GREEN}Successfully started Portainer.${NC}"
-echo "${GREEN}Portainer UI is available on {static_ip_address}:9000${NC}"
+echo $(date -u) "${GREEN}Successfully started Portainer.${NC}"
+echo $(date -u) "${GREEN}Portainer UI is available on {static_ip_address}:9000${NC}"
--- scripts/push-images.sh
+++ scripts/push-images.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 set -e
-# This script pushes EXISTING images to Docker Hub - this script is only meant for PBBG Maintainers to use!
-# Development Usage: ./push-images.sh --username myUserName --password myPassword
-# Production Usage: ./push-images.sh --username myUserName --password myPassword --production true
+# IMPORTANT: this script is intended to be used during the Github Actions build steps (using secret credentials)
+# This script pushes existing images to Docker Hub
+# Development Usage: ./push-images.sh --username=myUserName --password=myPassword
+# Production Usage: ./push-images.sh --username=myUserName --password=myPassword --production=true
 
 GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 CYAN='\033[1;36m'
 NC='\033[0m' # No Color
 
-echo "==== push-images.sh script ===="
+echo $(date -u) "==== push-images.sh script ===="
 while [ $# -gt 0 ]; do
   case "$1" in
     --username=*)
@@ -28,15 +29,15 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-echo "Logging into PBBG.com Docker Hub"
+echo $(date -u) "Logging into PBBG.com Docker Hub"
 echo $password | docker login -u $username --password-stdin
 
 if [ -n "$production" ]; then
-  echo "${CYAN}Pushing production images to PBBG.com Docker Hub${NC}"
+  echo $(date -u) "${CYAN}Pushing production images to PBBG.com Docker Hub${NC}"
   docker-compose -f docker-compose.build-for-prod.yml push
 else
-  echo "${YELLOW}Pushing development images to PBBG.com Docker Hub${NC}"
+  echo $(date -u) "${YELLOW}Pushing development images to PBBG.com Docker Hub${NC}"
   docker-compose push
 fi
 
-echo "${GREEN}Successfully pushed images to Docker Hub.${NC}"
+echo $(date -u) "${GREEN}Successfully pushed images to Docker Hub.${NC}"
