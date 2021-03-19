334ab5664add4baedcbb7407724eca8616de7ad1
Brian Kennedy <bpkennedy@gmail.com>
2021-03-19 16:37:35 -0500

    Create frontend lint rules and yarn tasks

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,14 +4,26 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+    'plugin:vue/strongly-recommended',
+    'eslint:recommended',
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
+    'generator-star-spacing': 'off',
+    'arrow-parens': 'off',
+    'one-var': 'off',
+
+    'import/first': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'no-trailing-spaces': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 }
