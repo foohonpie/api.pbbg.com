515b8efe018a24e6a6749cb1f38789a3970e1532
Brian Kennedy <bpkennedy@gmail.com>
2021-03-20 10:41:02 -0500

    Add Vue-Meta

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -38,21 +38,49 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld />
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+const metaSiteTitle = 'PBBG.com'
+const metaSiteTitleDescription = 'Persistent Browser Based Games'
+const metaDelimiter = ' | '
+const metaSiteDescription = 'PBBG.com is a free online directory of Persistent Browser Based Games, curated and reviewed by the players and developers within the PBBG community.'
+const metaSiteBaseUrl = 'https://dev.pbbg.com'
+const metaSiteImageUrl = 'https://dev.pbbg.com/pbbgSocialImage.png'
+const siteTitleAndTitleDescription = metaSiteTitle + metaDelimiter + metaSiteTitleDescription
 
 export default {
   name: 'App',
+  data: () => ({}),
+  metaInfo: {
+    title: `${metaSiteTitle}`,
+    titleTemplate: `${siteTitleAndTitleDescription}`,
+    meta: [
+      { name: 'description', content: metaSiteDescription },
 
-  components: {
-    HelloWorld,
+      // OpenGraph card
+      {property: 'og:title', content: siteTitleAndTitleDescription },
+      {property: 'og:site_name', content: metaSiteTitle },
+      {property: 'og:type', content: 'website'},
+      {property: 'og:url', content: metaSiteBaseUrl },
+      {property: 'og:image', content: metaSiteImageUrl },
+      {property: 'og:description', content: metaSiteDescription },
+
+      // Twitter card
+      {name: 'twitter:card', content: 'summary'},
+      {name: 'twitter:site', content: metaSiteBaseUrl },
+      {name: 'twitter:title', content: siteTitleAndTitleDescription },
+      {name: 'twitter:description', content: metaSiteDescription },
+
+      // Google / Schema.org markup:
+      {itemprop: 'name', content: siteTitleAndTitleDescription },
+      {itemprop: 'description', content: metaSiteDescription },
+      {itemprop: 'image', content: metaSiteImageUrl },
+    ],
   },
-
   data: () => ({
     test: 'test',
   }),
