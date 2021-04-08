3c3d5a3d1601ecac192397ff6359190451a156a6
Brian Kennedy <bpkennedy@gmail.com>
2021-04-08 16:45:07 -0500

    Add session authentication check to backend

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
+app.use(cors())
 app.use(serveStatic(path.join(__dirname, 'dist')))
 const port = 3000
 app.listen(port, function() {
